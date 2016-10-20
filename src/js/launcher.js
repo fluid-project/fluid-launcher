6a98e4624fed841eb899ee34999f4d1b17850161
Tony Atkins <tony@raisingthefloor.org>
2016-10-20 17:19:44 +0200

    Added JSDocs for all launcher functions.  Exposed the path resolution mechanism as a static function that can be reused in derived grades.

--- src/js/launcher.js
+++ src/js/launcher.js
@@ -9,6 +9,17 @@ var process = require("process");
 
 fluid.registerNamespace("gpii.launcher");
 
+/**
+ *
+ * Merge options from (in order of precedence):
+ *
+ * 1. An options file, if supplied.
+ * 2. Environment variables.
+ * 3. Command line parameters.
+ *
+ * @param that - The `gpii.launcher` component itself.
+ *
+ */
 gpii.launcher.mergeOptions = function (that) {
     fluid.each(that.options.yargsOptions, function (fnArgs, fnName) {
         yargs[fnName].apply(yargs, fluid.makeArray(fnArgs));
@@ -26,6 +37,16 @@ gpii.launcher.mergeOptions = function (that) {
     that.events.onOptionsMerged.fire(options);
 };
 
+/**
+ *
+ * Filter an object by a list of keys to include and/or a list of keys to exclude.
+ *
+ * @param includeKeys {Array} - The list of keys to include.
+ * @param excludeKeys {Array} - The list of keys to exclude.
+ * @param objectToFilter {Object} - The original object to filter.
+ * @returns {Object} The filtered object.
+ *
+ */
 gpii.launcher.filterKeys = function (includeKeys, excludeKeys, objectToFilter) {
     var filteredResults = fluid.copy(objectToFilter);
 
@@ -36,8 +57,28 @@ gpii.launcher.filterKeys = function (includeKeys, excludeKeys, objectToFilter) {
     return filteredResults;
 };
 
+/**
+ *
+ * Resolve a full, cwd-relative, or package-relative path to a full path.
+ *
+ * @param pathToResolve {String} - The path to resolve.
+ * @returns {String} - The resolved path
+ *
+ */
+gpii.launcher.resolvePath = function (pathToResolve) {
+    return path.resolve(process.cwd(), fluid.module.resolvePath(pathToResolve));
+};
+
+/**
+ *
+ * Load a JSON options file from `filePath`.
+ *
+ * @param filePath {String} - A full, cwd-relative, or package-relative path to an options file.
+ * @returns {Object} - The object represented by the JSON file.
+ *
+ */
 gpii.launcher.loadFileOptions = function (filePath) {
-    var fullPath = path.resolve(process.cwd(), fluid.module.resolvePath(filePath));
+    var fullPath = gpii.launcher.resolvePath(filePath);
     return require(fullPath);
 };
 
